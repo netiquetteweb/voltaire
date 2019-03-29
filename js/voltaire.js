3d077e603af279700a93213f5053c9b193094bd3
Suren <surendrathakur88@gmail.com>
2019-03-29 15:40:13 +0530

    profile update.

--- js/voltaire.js
+++ js/voltaire.js
@@ -3,4 +3,11 @@ jQuery(document).ready(function(){
   jQuery('.navbutton').click(function() {
     jQuery('body').toggleClass('show-nav');
   });
+  jQuery('#tokenfield').tokenfield({
+    autocomplete: {
+      source: ['Diving','Driving','Dating','Dancing','Cricket'],
+      delay: 100
+    },
+    showAutocompleteOnFocus: true
+  })
 });
